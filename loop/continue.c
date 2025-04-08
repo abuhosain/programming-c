d8e5ca2ea959b8a448007ec6963e4ec0a7a616cd
Hosain <cryptichasan925@gmail.com>
2025-04-08 23:21:39 +0600

    continue done

---
+++ loop/continue.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        if (i == 3)
+        {
+           continue;
+        }
+        printf("%d\n", i);
+    }
+    return 0;
+}
