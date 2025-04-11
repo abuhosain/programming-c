cffe2511af16305a292c68a88efde55d290d9999
Hosain <cryptichasan925@gmail.com>
2025-04-11 11:59:57 +0600

    problem 1

---
+++ assignment1/problem1.c
@@ -0,0 +1,19 @@
+/**
+ * Hello, world! I am learning C programming language. ^_^
+
+Programming is fun and challenging. /\/\/\
+
+I want to give my 100% dedication to learn!	I will succeed one day.
+Note: Here you will see 4 spaces in the last line which is a tab, you need to print a tab there.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, world! I am learning C programming language. ^_^\n");
+    printf("Programming is fun and challenging. /\\/\\/\\\n");
+    printf("I want to give my 100%% dedication to learn!\tI will succeed one day.");
+}
