ee893905f674285ef0e6947c6ef78b811ed392a6
Hosain <cryptichasan925@gmail.com>
2025-04-18 01:17:47 +0600

    is it challenge solve

---
+++ assignment2/is_it_challenge.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+   int n; 
+   scanf("%d", &n);
+   if(n > 0){
+    for(int i = 1; i <= n; i++ ){
+        printf("%d ", i);
+    }
+   }else{
+     while (n <= 0)
+     {
+        printf("%d ", n);
+        n++;
+     }
+     
+   }
+   return 0;
+};
