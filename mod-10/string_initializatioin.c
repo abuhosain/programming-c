ed25d304bcb4eebdfc4a44aa67b4fbf03783270b
Hosain <cryptichasan925@gmail.com>
2025-04-21 00:54:30 +0600

    string initialization

---
+++ mod-10/string_initializatioin.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  //  char s[5] = {
+  //     'a', 'b', 'c', 'd' 
+  //  };
+
+  char s[5] = "abcd"; // string initialization
+
+   printf("%s\n", s);
+   return 0;
+};
