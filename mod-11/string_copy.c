36a0b2c03b1e49a48774461c13b98feb01040aca
Hosain <cryptichasan925@gmail.com>
2025-04-23 00:37:53 +0600

    built in fucntion use for string copy

--- mod-11/string_copy.c
+++ mod-11/string_copy.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int main(){
    char a[100], b[100];
    scanf("%s %s", a, b);
-   int length = strlen(b);
+   // int length = strlen(b);
 
-   for(int i = 0; i <= length; i++){
-      a[i] = b[i];
-   }
+   // for(int i = 0; i <= length; i++){
+   //    a[i] = b[i];
+   // }
+   strcopy(a, b);
+
    printf("%s %s\n", a, b);
    return 0;
 };
