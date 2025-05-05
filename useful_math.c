926176d442ee0289c01313e860d5a1c28932316f
Hosain <cryptichasan925@gmail.com>
2025-05-05 14:49:30 +0600

    floorcd

--- useful_math.c
+++ useful_math.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-   int ans = ceil(2.5);
-   printf("The ceil value is: %d\n", ans);
+   int ceil_val = ceil(2.5);
+   printf("The ceil value is: %d\n", ceil_val);
    int floor_val = floor(2.5);
    printf("The floor value is: %d\n", floor_val);
+  int round_val = round(2.5);
+   printf("The round value is: %d\n", round_val);
    return 0;
 };
