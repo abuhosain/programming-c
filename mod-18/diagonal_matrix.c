04ce2c9774b7894e746e1ff5f9d6ec8bb814d66e
Hosain <cryptichasan925@gmail.com>
2025-05-20 01:16:43 +0600

    diagonal matrix

---
+++ mod-18/diagonal_matrix.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+  int r, c;
+  scanf("%d %d", &r, &c);
+  int arr[r][c];
+
+  for (int i = 0; i < r; i++)
+  {
+    for (int j = 0; j < c; j++)
+    {
+      scanf("%d", &arr[i][j]);
+    }
+  }
+int flag = 0;
+  if (r == c)
+  {
+    for (int i = 0; i < r; i++)
+    {
+      for (int j = 0; j < c; j++)
+      {
+        if (i == j)
+        {
+           
+        }
+        else
+        if(arr[i][j] != 0)
+        {
+          flag = 1;
+          printf("NO");
+          return 0;
+        }
+      }
+    }
+   if(flag == 0)
+   {
+    printf("YES");
+   }
+  }
+  else
+  {
+    printf("NO");
+  }
+};
